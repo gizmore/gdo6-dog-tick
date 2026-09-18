bc67811b5acfd7183ba6c3046b0425e6ebda5fcd
gizmore <gizmore@wechall.net>
2026-09-18 04:08:06 +0200

    'more korean'

---
+++ lang/tick_ko.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+namespace GDO\DogTick\lang;
+return [
+	'dog_help_tick_init' => '자신에게 초기 감염을 만듭니다.',
+	'err_dog_tick_init_failed' => '당신은 이미 감염되었습니다.',
+	'msg_dog_tick_init_succeeded' => '초기 감염이 발생했습니다.',
+	'dog_help_tick' => '사용자를 코로나 질병에 감염시킵니다.',
+	'err_dog_no_disease' => '아직 코로나는 없으시군요.',
+	'err_dog_already_ticked' => '%s은(는) %s에 이미 %s에 감염되었습니다.',
+	'mutated' => '돌연변이됐어요!',
+	'not_mutated' => '정상인 것 같습니다.',
+	'msg_dog_ticked' => '%s 포인트에 대해 %s을(를) 선택했습니다. 이제 코로나 변종 %s - %s이(가) 있습니다. %s명을 감염시켰습니다!',
+	'msg_dog_you_are_infected' => '%s님이 %s 코로나에 감염되었습니다! 당신은 그의 피해자 번호 %s입니다. "tick" 명령으로 다른 사람을 감염시킬 수 있습니다.',
+	'dog_help_ticked' => '사용자가 감염되었는지 확인하세요.',
+	'msg_dog_not_infected' => '%s은(는) 아직 감염되지 않았습니다.',
+	'msg_dog_infected' => '%s은(는) 의도적으로 %s에 의해 %s에 감염된 불쌍한 영혼입니다!',
+	'dog_help_tickstats' => '코로나 게임 통계를 표시합니다.',
+	'msg_dog_tickstats_total' => '이번 코로나 게임에서는 %s명이 감염되었습니다. 최고의 플레이어인 %s이(가) %s명을 감염시켰습니다.',
+	'msg_dog_tickstats_top10' => '%s 코로나 플레이어, 페이지 %s/%s: %s.',
+	'msg_dog_tickstats_victims' => '%s의 최근 코로나 피해자, 페이지 %s/%s: %s.',
+	'dog_help_tickstats_user' => '코로나 게임 사용자에 대한 통계를 표시합니다.',
+	'msg_dog_tickstats_user_nope' => '%s은(는) 누구도 감염시키지 않았으며 %s번 틱을 받았습니다. %s',
+	'msg_dog_tickstats_user' => '%s은(는) %s명을 감염시켰으며 %s번 틱을 받았습니다. %s',
+	'has_corona' => '코로나 변종 %s이(가) 있습니다!',
+	'no_corona' => '그 사람들은 코로나가 없어요.',
+	'dog_help_tick_reset' => '게임을 완전히 재설정하세요. 모든 진행 상황이 손실됩니다.',
+	'msg_tick_reset' => '게임이 재설정되었습니다.',
+];
